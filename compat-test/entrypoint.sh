48678dfabfeb0628c9b2cd0fa6599a6ab2b0c322
legobt <6wbvkn0j@anonaddy.me>
2023-12-04 04:17:20 +0000

    wip: compat testing with npm in compat-test

--- compat-test/entrypoint.sh
+++ compat-test/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -e
+set -ex
 
-local_registry="http://0.0.0.0:4873"
+local_registry="http://127.0.0.1:4873"
 
 # start local registry
 tmp_registry_log=`mktemp`
@@ -14,5 +14,17 @@ sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_regis
 # grep -q 'http address' <(tail -f $tmp_registry_log)
 # login so we can publish packages
 sh -c "npm-auth-to-token -u test -p test -e test@test.com -r $local_registry"
-# Run nmp command
+# Run npm command
 sh -c "npm publish --registry $local_registry $1"
+
+
+## NPM
+
+mkdir "$HOME/app"
+cd "$HOME/app"
+npm init
+npm set registry "$local_registry"
+npm install "$1"
+ls "./node_modules/$1/package.json"
+
+#sh -c "${@}"
