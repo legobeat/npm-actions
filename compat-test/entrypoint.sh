f21e1ad585177e6b256708443483d279677a26c7
legobt <6wbvkn0j@anonaddy.me>
2023-12-04 06:12:59 +0000

    dynamic verdaccio config

--- compat-test/entrypoint.sh
+++ compat-test/entrypoint.sh
@@ -3,14 +3,15 @@
 set -ex
 
 local_registry="http://127.0.0.1:4873"
-pkg_name=$(jq -r .name ./package.json)
-pkg_scope=$(echo $pkg_name | grep -o '^@[^/]*'; true)
+export pkg_name=$(jq -r .name ./package.json)
+export pkg_scope=$(echo $pkg_name | grep -o '^@[^/]*'; true)
 
 # start local registry
 tmp_registry_log=`mktemp`
-sh -c "mkdir -p $HOME/.config/verdaccio"
-sh -c "cp --verbose /config.yaml $HOME/.config/verdaccio/config.yaml"
+mkdir -p $HOME/.config/verdaccio
+cat /verdaccio-config.yaml.tmpl | envsubst '$pkg_name' | tee $HOME/.config/verdaccio/config.yaml
 sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_registry_log &"
+
 # wait for `verdaccio` to boot
 # FIXME: this throws a syntax error, but would be great to make it run
 # grep -q 'http address' <(tail -f $tmp_registry_log)
